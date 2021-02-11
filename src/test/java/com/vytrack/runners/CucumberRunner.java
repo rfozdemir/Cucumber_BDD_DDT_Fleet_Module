55e0973202cc3251ab7652793d32a5fd8c63025d
rfurkan2367 <rfurkan2367@gmail.com>
2021-02-11 00:35:12 -0500

    first commit

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,23 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@negative_test",
+        plugin = {"html:target/default-cucumber-reports",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+        }
+)
+public class CucumberRunner {
+
+}
+
+//if you are asked during interview how do you make your tests faster, How would you improve test execution time
+// you will answer first of all we have to maximize usage of explicit and fluent wait and if it is possible I would use chrome headless
